bb13af24ce85f842de1b315ea66080a91c04ebff
juhnoktober968 <juhnoktober968@gmail.com>
2025-10-07 11:54:19 +0800

    fix routes for login and register

--- csucc_capstone_management_system/app/views.py
+++ csucc_capstone_management_system/app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'public/index.html')
+
+def login(request):
+    return render(request, 'public/login.html')
+
+def register(request):
+    return render(request, 'public/register.html')
